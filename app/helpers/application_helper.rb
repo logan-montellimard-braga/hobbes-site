bc992f78352c7cdfe70ab2eae82111c0baf9e18e
Logan Braga <braga.logan@gmail.com>
2014-12-25 23:43:50 +0100

    Navbar links now depends on authentication status

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,26 +3,21 @@ module ApplicationHelper
     link_to image_tag('hobbes_logo_only.png', :class => klass), root_path
   end
 
-  def nav_link(link_text, link_path)
+  def nav_link(link_text, link_path, method = :get)
     class_name = current_page?(link_path) ? 'active' : nil
 
     content_tag(:li) do
-      link_to link_text, link_path, :class => class_name
+      link_to link_text, link_path, :class => class_name, :method => method
     end
   end
 
-  def dropdown_button(link_text, link_path, classes, children_paths)
-    klass = nil
-
-    children_paths.each do |p|
-      if current_page?(p)
-        klass = 'active'
-        break
-      end
-    end
-
-    content_tag(:li) do
-      link_to link_text, link_path, :class => klass.to_s + " " + classes, :'data-activates' => 'main_drop'
+  def nav_link_member
+    if user_signed_in?
+      return (nav_link "Profil", "#!") +
+        (nav_link "Se déconnecter", destroy_user_session_path, :delete)
+    else
+      return (nav_link "Se connecter", new_user_session_path) +
+        (nav_link "S'inscrire", new_user_registration_path)
     end
   end
 end
